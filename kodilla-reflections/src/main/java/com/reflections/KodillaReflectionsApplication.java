995b21baf8d02634d6325504c40a26bee72f29fa
Rafal-Kodilla <rjanus@wip.pl>
2017-10-18 08:38:50 +0200

    Moduł 18.2

--- kodilla-reflections/src/main/java/com/reflections/KodillaReflectionsApplication.java
+++ kodilla-reflections/src/main/java/com/reflections/KodillaReflectionsApplication.java
@@ -3,6 +3,7 @@ package com.reflections;
 import com.reflections.annotations.TablesAndColumnsCreator;
 import com.reflections.annotations.MyAnnotation;
 import com.reflections.pairs.GenericClassExecutor;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.sql.SQLException;
@@ -11,7 +12,7 @@ import java.sql.SQLException;
 public class KodillaReflectionsApplication {
 
 	public static void main(String[] args) throws SQLException {
-		//SpringApplication.run(KodillaReflectionsApplication.class, args);
+		SpringApplication.run(KodillaReflectionsApplication.class, args);
 		GenericClassExecutor.genericClassExecuting();
 		System.out.println();
 		MyAnnotation.testingAnnotations();
---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,14 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Getter
+public class TrelloBoardDto {
+
+    private String id;
+    private String name;
+}
