3627e15b59926f8b02e81557368fc312899a5ad7
Rafał Janus <RJanus@wip.pl>
2017-10-29 13:52:43 +0100

    Heroku

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -1,7 +1,6 @@
 package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
-import com.mysql.fabric.xmlrpc.base.Param;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
